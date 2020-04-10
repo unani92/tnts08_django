6517f52238c159d543f2e6eb69dc84854b38f3d1
unani <unani92@naver.com>
2020-04-10 19:15:20 +0900

    pagination_almost_fin

--- board/views.py
+++ board/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Board
 from .forms import BoardForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request) : 
-    boards = Board.objects.order_by('-pk')
+    boards_list = Board.objects.order_by('-pk')
+    paginator = Paginator(boards_list,2)
+    page = request.GET.get('page')
+
+    try: 
+        boards = paginator.page(page)
+    except PageNotAnInteger : 
+        boards = paginator.page(1)
+    except EmptyPage : 
+        boards = paginator.page(paginator.num_pages)
+
     context = {'boards':boards}
+
+    print(boards.has_other_pages())
+
     return render(request, 'board/index.html', context)
 
 def create(request) : 

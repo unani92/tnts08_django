52211d2f6e4aa1bd648cd5fa18f8c6c4f69a39e8
unani <unani92@naver.com>
2020-04-10 13:17:27 +0900

    ckeditor_first_commit

--- board/models.py
+++ board/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 class Board(models.Model) : 
     title = models.CharField(max_length=30)
-    content = models.TextField()
+    content = RichTextUploadingField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 

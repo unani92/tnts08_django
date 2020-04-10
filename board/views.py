b79420d6390ecb45aeb68b9c0612217519126b26
unani <unani92@naver.com>
2020-04-10 23:48:19 +0900

    pagination_v3

--- board/views.py
+++ board/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 def index(request) : 
     boards_list = Board.objects.order_by('-pk')
-    paginator = Paginator(boards_list,2)
+    paginator = Paginator(boards_list,1)
     page = request.GET.get('page')
 
     try: 
@@ -16,9 +16,21 @@ def index(request) :
     except EmptyPage : 
         boards = paginator.page(paginator.num_pages)
 
-    context = {'boards':boards}
+    idx = boards.number-1
+    max_idx = len(paginator.page_range)
+    start_idx = idx-2 if idx >= 2 else 0
+    if idx < 2 : 
+        end_idx = 5-start_idx
+    else : 
+        end_idx = idx+3 if idx <= max_idx else max_idx
+    
+    page_range = list(paginator.page_range[start_idx:end_idx])
 
-    print(boards.has_other_pages())
+    context = {
+        'boards':boards,
+        'page_range':page_range,
+        'max_idx':max_idx-2,
+        }
 
     return render(request, 'board/index.html', context)
 

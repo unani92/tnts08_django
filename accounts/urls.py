c26ca1e6ac2b1e84b8759153e41e9d8ac316608e
unani <unani92@naver.com>
2020-04-18 18:15:49 +0900

    accounts_detail v1

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'), 
+    path('detail/', views.detail, name='detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import MyUserCreationForm
+from .models import MyUser
 # Create your views here.
 
 def signup(request) : 
     
     if request.method == 'POST' : 
         form = MyUserCreationForm(request.POST)
-        if form.is_valid() : 
+        if form.is_valid() and request.POST.get('agree1') and request.POST.get('agree2'): 
             form.save()
             return redirect('board:index')
     else : 
@@ -15,3 +16,14 @@ def signup(request) :
     context = {'form':form}
 
     return render(request,'accounts/signup.html',context)
+
+def detail(request) : 
+    if request.GET.get('firstname') : 
+        search = request.GET.get('firstname')
+        users = MyUser.objects.filter(first_name=search)
+    
+    else : 
+        users = MyUser.objects.order_by('first_name')
+
+    context = {'users':users}
+    return render(request,'accounts/detail.html',context)

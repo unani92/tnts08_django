178b9f38eca1741fe3c7690a1876f1873fafe752
unani <unani92@naver.com>
2020-05-03 23:26:11 +0900

    calender_join & dismiss : block overlapping

--- calender/views.py
+++ calender/views.py
@@ -28,22 +28,39 @@ def detail(request,pk):
     return render(request,'calender/detail.html',context)
 
 
-# 참전 불참 중복 방지 기능 추가
 # 참전 인원 공유하는 드롭다운 개설
     
 def joinmatch(request,pk):
+    response = redirect('calender:detail', pk)
     match = get_object_or_404(JoinMatch,pk=pk)
-    join = Join()
-    join.user = request.user
-    join.time = request.POST.get('time')
-    join.save()
-    match.join_match.add(join)
-    return redirect('calender:detail',pk)
+    if match.join_match.filter(user=request.user):
+        return response
+    else :
+        join = Join()
+        join.user = request.user
+        join.time = request.POST.get('time')
+        join.save()
+
+        if match.dismiss_match.filter(user=request.user):
+            dismiss = match.dismiss_match.get(user=request.user)
+            match.dismiss_match.remove(dismiss)
+
+        match.join_match.add(join)
+        return response
 
 def dismiss(request,pk):
+    response = redirect('calender:detail', pk)
     match = get_object_or_404(JoinMatch,pk=pk)
-    dismiss = Dismiss()
-    dismiss.user = request.user
-    dismiss.save()
-    match.dismiss_match.add(dismiss)
-    return redirect('calender:detail',pk)
+    if match.dismiss_match.filter(user=request.user):
+        return response
+    else :
+        dismiss = Dismiss()
+        dismiss.user = request.user
+        dismiss.save()
+
+        if match.join_match.filter(user=request.user):
+            join = match.join_match.get(user=request.user)
+            match.join_match.remove(join)
+
+        match.dismiss_match.add(dismiss)
+        return response

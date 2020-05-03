a5bf45041eaf6adce6aa8c0e214ef1a69ad8d4fe
unani <unani92@naver.com>
2020-05-03 18:56:24 +0900

    calender_join v1

--- calender/forms.py
+++ calender/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import JoinMatch, Join, Dismiss
-from tempus_dominus.widgets import DateTimePicker, TimePicker
+from tempus_dominus.widgets import DatePicker, TimePicker
 
 choice1 = [('home','home'),('away','away')]
 choice2 = [('League','League'),('ACL','ACL'),('FA','FA')]
@@ -15,7 +15,7 @@ class JoinMatchForm(forms.ModelForm):
         label='경기 종류'
     )
     date = forms.DateTimeField(
-        widget= DateTimePicker(
+        widget= DatePicker(
             options={
                 'minDate': '2020-01-01',
             }
@@ -27,3 +27,20 @@ class JoinMatchForm(forms.ModelForm):
     class Meta:
         model = JoinMatch
         fields = ['home_away','league_acl_fa','vs','highlight','date']
+
+class JoinForm(forms.ModelForm):
+    time = forms.TimeField(
+        widget=TimePicker(
+            options={
+                'enabledHours': list(range(0,24)),
+                'defaultDate': '1970-01-01T14:56:00'
+            },
+            attrs={
+                'input_toggle': True,
+                'input_group': True,
+            },
+        ),
+    )
+    class Meta:
+        model = Join
+        fields = ['time']
--- calender/urls.py
+++ calender/urls.py
@@ -4,4 +4,5 @@ app_name = 'calender'
 urlpatterns = [
     path('create/',views.create,name='create'),
     path('<int:pk>/', views.detail,name='detail'),
+    path('<int:pk>/joinmatch/',views.joinmatch,name='joinmatch'),
 ]

a1291c9cb5753b889e57c7ef151d8dcfdb3ec788
unani <unani92@naver.com>
2020-05-03 19:11:59 +0900

    calender_dismiss v1

---
+++ calender/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from datetime import datetime
+from django.conf import settings
+# Create your models here.
+
+class Join(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='join_users',
+        on_delete=models.CASCADE
+    )
+    time = models.TimeField()
+
+class Dismiss(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='dismiss_users',
+        on_delete=models.CASCADE
+    )
+
+class JoinMatch(models.Model):
+    home_away = models.CharField(max_length=10)
+    league_acl_fa = models.CharField(max_length=10)
+    vs = models.CharField(max_length=20)
+    highlight = models.BooleanField()
+    date = models.DateTimeField()
+    join_match = models.ManyToManyField(
+        Join,
+        related_name='join_matches'
+    )
+    dismiss_match = models.ManyToManyField(
+        Dismiss,
+        related_name='dismiss_matches'
+    )
+
+    def d_day(self):
+        return self.date.day - datetime.now().day

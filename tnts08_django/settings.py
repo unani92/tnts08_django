d3cd28b05a15ca88a8a8a953c609a4d5cf92e732
unani <unani92@naver.com>
2020-05-05 18:21:57 +0900

    heroku-setting

--- tnts08_django/settings.py
+++ tnts08_django/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n%t%t40+le(ip9$b4lhk55(gjf$^vity(fb5n2q*a_=x-+dz9p'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','n%t%t40+le(ip9$b4lhk55(gjf$^vity(fb5n2q*a_=x-+dz9p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 

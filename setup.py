9322f18b20b4f3b398cc28d01c3e4be2aa7bec09
unani <unani92@naver.com>
2020-05-06 17:07:56 +0900

    heroku

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tnts08_django',
+    version='',
+    packages=['home', 'home.migrations', 'board', 'board.migrations', 'board.templatetags', 'accounts',
+              'accounts.migrations', 'calender', 'calender.migrations', 'tnts08_django'],
+    url='',
+    license='',
+    author='unani',
+    author_email='',
+    description=''
+)

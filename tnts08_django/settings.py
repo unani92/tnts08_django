117556a8032ac8f45849028d1039a24d63db4bc0
unani <unani92@naver.com>
2020-05-09 00:32:12 +0900

    video upload bug fix

--- tnts08_django/settings.py
+++ tnts08_django/settings.py
@@ -198,5 +198,6 @@ CLOUDINARY_STORAGE = {
     'API_SECRET':get_secret('CLOUDINARY_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
+# 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
